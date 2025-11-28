5814045cee4f49e79cae04d2185e57c9f3f012f8
piyusha <piyusha.bodakhe@yash.com>
2025-11-28 14:54:47 +0530

    updated frontend logic

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import Login from "./component/Login";
 import Register from "./component/Registration";
 import ForgotPassword from "./component/ForgotPassword";
 import AIFixes from "./component/AiFixes";
-import LogAnalyzer from "./component/LogAnalyzer"; // ✅ Your moved code goes here
 import Dashboard from "./pages/Dashboard";
 import Logs from "./pages/Logs"
 import Navbar from "./component/Navbar";
